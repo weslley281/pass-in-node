b96844de20086fee4fdc039ca5e796854acb496f
Weslley Ferraz <weslleyhenrique800@gmail.com>
2024-04-15 14:30:14 -0400

    http

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,41 @@
 import fastify from 'fastify';
+import {z} from 'zod';
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify();
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 const port = 5000;
 
-app.get('/', () => {
-  return 'Weslley você é foda';
+app.get('/', (request, reply)=> {
+  return "Olá povo"
+});
+
+app.post('/events', async (request, reply)=> {
+  const creatEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable()
+  });
+
+  const data = creatEventSchema.parse(request.body);
+
+  const titleWords = data.title.toLowerCase().split(" ");
+  const slug = titleWords.join("-");
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      slug,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees
+    }
+  })
+
+  return reply.status(201).send({ event })
+  //return { eventId: event.id }
 });
 
 app.listen({ port }).then(() => {
